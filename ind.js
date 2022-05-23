f44429c5c12a945ae3736cd0b72880cb76784189
GulnazKayayeva <yuupuu579@gmail.com>
2022-05-24 04:02:55 +0500

    nn

--- ind.js
+++ ind.js
@@ -1,28 +1,42 @@
 let body = document.querySelector('body')
 let it = document.createElement('div')
+let it2 = document.createElement('div')
 let p = document.createElement('div')
 let p2 = document.createElement('div')
 let h1 = document.createElement('h1')
 let form = document.forms.todo
+let container = document.createElement('div')
+let done_h1 = document.createElement('h2')
+let done_h2 = document.createElement('h2')
+done_h1.innerHTML = 'Not Done!'
+done_h2.innerHTML = 'Done!'
+
+
+
+let cont1 = document.createElement('div')
+let cont2 = document.createElement('div')
+
+cont1.classList.add('newww')
+cont2.classList.add('newww')
+
+
+
+cont1.append(done_h1)
+cont2.append(done_h2)
+container.append(cont1, cont2)
+container.classList.add('cont')
+
+
+
 
 it.classList.add('ass')
-
+it2.classList.add('ass')
 body.prepend(h1)
 h1.innerHTML = 'Todo List'
 
-let todos = [{
-    "id": 1,
-    "task": "купить дом на марсе",
-    "status": false,
-    "time": "10:10"
-},
-{
-    "id": 2,
-    "task": "купить машину",
-    "status": false,
-    "time": "10:10"
-}
-]
+let todos = [] || JSON.parse(localStorage.getItem('todos')) 
+
+let todos_done = []
 
 form.onsubmit = (event) => {
     event.preventDefault()
@@ -48,11 +62,13 @@ form.onsubmit = (event) => {
         reload(todos)
     }
 
+    localStorage.todos = JSON.stringify(todos)
 }
 
 
 let reload = (arr) => {
     it.innerHTML = ''
+    it2.innerHTML = ''
     console.log(arr);
     for (let item of arr) {
         let newTask = document.createElement('div')
@@ -61,24 +77,78 @@ let reload = (arr) => {
         let del = document.createElement('img')
         let box = document.createElement('div')
         let time = document.createElement('p')
+        let checkbox = document.createElement('input')
+        let done = document.createElement('div')
+        let done_text = document.createElement('span')
 
+
+
+
+
+        done_text.innerHTML = 'not done!'
+        done_text.classList.add('notDone')
+        checkbox.type = 'checkbox'
+        done.style.margin = '10px 0 0 0'
         del.src = "./done.svg"
         box.classList.add('box')
         h.innerHTML = item.task
         time.innerHTML = item.time
 
         box.append(h, del)
-        newTask.append(box, time)
-        it.append(newTask)
+        done.append(checkbox, done_text)
+        newTask.append(box, time, done)
+        if (item.status) {
+            done.innerHTML = ''
+            done_text.innerHTML = 'done!'
+            done_text.classList.add('done')
+            it2.append(newTask)
+            cont2.append(it2)
+            done.append(done_text)
+        } else {
+            it.append(newTask)
+            cont1.append(it)
+        }
+
+
+        checkbox.onclick = () => {
+
+            if (!checkbox.checked) {
+                done_text.classList.remove('done')
+                done_text.innerHTML = 'not done!'
+                done_text.classList.add('notDone')
+                item.status = false
+            
+
+            } else {
+                done_text.classList.remove('notDone')
+                done_text.innerHTML = 'done!'
+                done_text.classList.add('done')
+                // it2.append(newTask)
+                item.status = true
+                // cont2.append(it2)
+            
+            }
+            localStorage.todos = todos
+            reload(arr)    
+        }
 
         del.onclick = () => {
+
             arr.splice(arr.indexOf(item), 1)
             console.log(arr);
-            reload(todos)
+            localStorage.todos = todos
+            reload(arr)
         }
     }
 }
 
-body.append(it)
-reload(todos)
+
+
+body.append(container)
+
+let temp = JSON.parse(localStorage.getItem('todos'))
+
+if(temp.length > 0){
+    reload(temp)
+}
 
